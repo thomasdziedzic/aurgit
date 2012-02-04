9e4b88063f39825edeec6f892ac082d101baf514
Thomas Dziedzic <gostrc@gmail.com>
2012-02-04 05:55:19 +0000

    use the old script and modify it a bit to help maintain a clean git tree

--- update-aur.sh
+++ update-aur.sh
@@ -2,19 +2,19 @@
 
 # CONFIG
 AURGITCLONE=/home/td123/tmp/aurgitclone
-AURTEST=/home/td123/tmp/aurtest
+AURTEST=/home/td123/tmp/aursrcpkgs
 
 # SANITY CHECKS
 [ ! -d $AURGITCLONE ] && echo "run: git clone /srv/git/aur.git/ $AURGITCLONE"j&& exit 1
 [ ! -d $AURTEST ] &&  echo "run: install -d $AURTEST" && exit 1
 
-# HARDCORE SYNCING
+# Syncing *.src.tar.gz
 cd $AURTEST
 rm -f /tmp/rlog
 rsync -avz --include '/*.tar.gz' --exclude '/*' --exclude '/.git/' --temp-dir '/tmp' --inplace --update --delete \
-      'rsync://aur.archlinux.org/unsupported/*/' . |& tee /tmp/rlog
+      'rsync://aur.archlinux.org/unsupported/*/*/*.tar.gz' . |& tee /tmp/rlog
 
-# SOFTCORE PROCESSING
+# Process all the stuff
 cd $AURGITCLONE
 while read line; do
   match=$(echo $line | sed -n 's/^deleting \(.*\).tar.gz/\1/p')
@@ -27,14 +27,16 @@ while read line; do
     if [[ x$match != x ]]; then
       #echo updated
       rm -rf ${match}
+      mkdir ${match}
+      bsdtar -s ',.*/,,p' -C ${match} -xf $AURTEST/${match}.tar.gz
       bsdtar xf $AURTEST/$match.tar.gz
     fi
   fi
 done < /tmp/rlog
 rm -f /tmp/rlog
 
-# GIT ER DONE
-git add *
+# GIT STUFF
+git add -A 
 git commit -am "updated on $(date)"
-git push
-
+git gc --auto
+git push origin master
