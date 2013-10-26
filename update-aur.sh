8ce6b699dff144fdad9807a718bf0d2007dfa90d
Thomas Dziedzic <gostrc@gmail.com>
2013-10-26 12:48:16 -0700

    commit changes that Ive made over the years

--- update-aur.sh
+++ update-aur.sh
@@ -4,17 +4,20 @@
 AURGITCLONE=/home/td123/tmp/aurgitclone
 AURTEST=/home/td123/tmp/aursrcpkgs
 
+RSYNC_HOST='aur.archlinux.org'
+
 # SANITY CHECKS
-[ ! -d $AURGITCLONE ] && echo "run: git clone /srv/git/aur.git/ $AURGITCLONE"j&& exit 1
+[ ! -d $AURGITCLONE ] && echo "run: git clone /srv/git/aur-mirror.git/ $AURGITCLONE" && exit 1
 [ ! -d $AURTEST ] &&  echo "run: install -d $AURTEST" && exit 1
 
 # Syncing *.src.tar.gz
 cd $AURTEST
 rm -f /tmp/rlog
 rsync -avz --include '/*.tar.gz' --exclude '/*' --exclude '/.git/' --temp-dir '/tmp' --inplace --update --delete \
-      'rsync://aur.archlinux.org/unsupported/*/*/*.tar.gz' . |& tee /tmp/rlog
+      "rsync://${RSYNC_HOST}/unsupported/*/*/*.tar.gz" . |& tee /tmp/rlog || (echo 'rsync failed' && exit 1)
 
-# Process all the stuff
+echo 'extracting package files'
+
 cd $AURGITCLONE
 while read line; do
   match=$(echo $line | sed -n 's/^deleting \(.*\).tar.gz/\1/p')
@@ -33,6 +36,8 @@ while read line; do
   fi
 done < /tmp/rlog
 rm -f /tmp/rlog
+
+echo 'adding files to git'
 
 # GIT STUFF
 git add -A 
